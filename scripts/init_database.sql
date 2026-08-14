c5ad98439411ea0e0398314fbb4da7d792e286cd
Ajay T <ajayarya3803@gmail.com>
2026-08-14 11:34:01 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,8 @@
+drop database if exists DataWarehouse; Drops the database
+create Database DataWarehouse; Creates
+use DataWarehouse; uses a Database
+  
+  -- create schema - it's a container/folder to organise the data 
+ create schema bronze;
+ create schema silver;
+ create schema gold;
